aa66e5729c7732f8934da6f5b79b8377115ba4bb
Navaneet R Rao <navaneet517@gmail.com>
2024-06-23 13:27:23 +0530

    feat: keymaps fuzzy finder

--- lua/luke/core/keymaps.lua
+++ lua/luke/core/keymaps.lua
@@ -2,6 +2,9 @@
 -- My Key Maps --
 -----------------
 
+vim.keymap.set("n", "<leader>/", "<CR>:Telescope keymaps<CR>", {})
+
+-- --
 vim.keymap.set("n", "<leader>E", ":E<CR>", {})
 vim.keymap.set("i", "<leader>jj", "<esc>", {})
 -- Neo Tree --
@@ -30,11 +33,11 @@ vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
 vim.keymap.set("n", "<space>wl", function()
-  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 end, opts)
 
 vim.keymap.set("n", "<space>f", function()
-  vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = true })
 end, opts)
 
 -- Undo Tree --
@@ -44,28 +47,28 @@ vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
 -- Trouble --
 
 vim.keymap.set("n", "<leader>tt", function()
-  require("trouble").toggle()
+	require("trouble").toggle()
 end)
 vim.keymap.set("n", "<leader>tn", function()
-  require("trouble").next({
-    skip_groups = true,
-    jump = true,
-  })
+	require("trouble").next({
+		skip_groups = true,
+		jump = true,
+	})
 end)
 vim.keymap.set("n", "<leader><F6>", function()
-  require("trouble").toggle("workspace_diagnostics")
+	require("trouble").toggle("workspace_diagnostics")
 end)
 vim.keymap.set("n", "<leader>xd", function()
-  require("trouble").toggle("document_diagnostics")
+	require("trouble").toggle("document_diagnostics")
 end)
 vim.keymap.set("n", "<leader>xq", function()
-  require("trouble").toggle("quickfix")
+	require("trouble").toggle("quickfix")
 end)
 vim.keymap.set("n", "<leader>xl", function()
-  require("trouble").toggle("loclist")
+	require("trouble").toggle("loclist")
 end)
 vim.keymap.set("n", "gR", function()
-  require("trouble").toggle("lsp_references")
+	require("trouble").toggle("lsp_references")
 end)
 
 -- none ls --
@@ -81,29 +84,29 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():append()
+	harpoon:list():append()
 end)
 vim.keymap.set("n", "<C-e>", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
 vim.keymap.set("n", "<C-h>", function()
-  harpoon:list():select(1)
+	harpoon:list():select(1)
 end)
 vim.keymap.set("n", "<C-t>", function()
-  harpoon:list():select(2)
+	harpoon:list():select(2)
 end)
 vim.keymap.set("n", "<C-n>", function()
-  harpoon:list():select(3)
+	harpoon:list():select(3)
 end)
 vim.keymap.set("n", "<C-s>", function()
-  harpoon:list():select(4)
+	harpoon:list():select(4)
 end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function()
-  harpoon:list():prev()
+	harpoon:list():prev()
 end)
 vim.keymap.set("n", "<C-S-N>", function()
-  harpoon:list():next()
+	harpoon:list():next()
 end)
