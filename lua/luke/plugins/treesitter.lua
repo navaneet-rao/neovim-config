f9809b4be3ae47fd83c31dd237f2bd263effc835
Navaneet R Rao <navaneet517@gmail.com>
2024-08-07 14:20:33 +0530

    fix: i have do idea how this is working :)

--- lua/luke/plugins/treesitter.lua
+++ lua/luke/plugins/treesitter.lua
@@ -11,6 +11,8 @@ local treesitter = {
 		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = true,
 				ensure_installed = {
 					"c",
 					"lua",
@@ -24,24 +26,6 @@ local treesitter = {
 					"tsx",
 					"json",
 					"css",
-				},
-
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-				rainbow = {
-					enable = true,
-					extended_mode = true,
-				},
-				autotag = {
-					enable = true,
-				},
-				context_commentstring = {
-					enable = true,
 				},
 			})
 		end,
