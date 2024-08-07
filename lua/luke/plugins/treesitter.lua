810b3f47e4ee18ae474fbb21781f33c5620603fd
Navaneet R Rao <navaneet517@gmail.com>
2024-08-07 14:01:43 +0530

    feat: remove changes

--- lua/luke/plugins/treesitter.lua
+++ lua/luke/plugins/treesitter.lua
@@ -11,6 +11,8 @@ local treesitter = {
 		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = true,
 				ensure_installed = {
 					"c",
 					"lua",
